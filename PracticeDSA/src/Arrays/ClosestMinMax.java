bee8b17bb327cb62c7454f7b67cf370d3802c9b0
ManishKshetrapal45 <manishkshetrapal105@gmail.com>
2025-10-01 19:59:51 +0530

    Code for problem closest min max

--- PracticeDSA/src/Arrays/ClosestMinMax.java
+++ PracticeDSA/src/Arrays/ClosestMinMax.java
@@ -26,14 +26,48 @@ Output 2:
 
 Example Explanation:
 Explanation 1:
- Take the 1st and 2nd elements as they are the minimum and maximum elements respectievly.
+ Take the 1st and 2nd elements as they are the minimum and maximum elements respectively.
 Explanation 2:
  Take the last 3 elements of the array.
 */
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClosestMinMax {
     public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>(List.of(2, 6, 1, 6, 9));
+        System.out.println(solve(A));
+    }
 
+    public static int solve(ArrayList<Integer> A) {
+        int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
+        for (Integer integer : A) {
+            if (max < integer) {
+                max = integer;
+            }
+            if (min > integer) {
+                min = integer;
+            }
+        }
+        int N=A.size(),ans=A.size(),maxI=-1,minI=-1,l;
+        for(int i=N-1;i>=0;i--){
+            if(A.get(i)==min){
+                minI=i;
+                if(maxI!=-1){
+                    l=Math.abs(minI-maxI)+1;
+                    ans=Math.min(ans,l);
+                }
+            }
+            if(A.get(i)==max){
+                maxI=i;
+                if(minI!=-1){
+                    l=Math.abs(minI-maxI)+1;
+                    ans=Math.min(ans,l);
+                }
+            }
+        }
+        return(ans);
     }
 }
