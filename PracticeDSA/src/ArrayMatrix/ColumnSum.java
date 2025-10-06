2901e60f325445dd250ba95a90347d313fa82425
ManishKshetrapal45 <manishkshetrapal105@gmail.com>
2025-10-06 21:20:33 +0530

    Code for problem Add the matrix and Column Sum

--- PracticeDSA/src/ArrayMatrix/ColumnSum.java
+++ PracticeDSA/src/ArrayMatrix/ColumnSum.java
@@ -33,36 +33,14 @@ Column 4 = 4+8+4 = 16
 package ArrayMatrix;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
-public class AddTheMatrices {
+public class ColumnSum {
     public static void main(String[] args) {
         List<List<Integer>> A = new ArrayList<>();
         A.add(Arrays.asList(1, 2, 3));
         A.add(Arrays.asList(4, 5, 6));
         A.add(Arrays.asList(7, 8, 9));
-
-        List<Integer> ans = solve(A);
-        for (int i = 0; i < ans.size(); i++)
-        {
-            System.out.println(ans.get(i) + " ");
-        }
-    }
-
-    public static List<Integer> solve(List<List<Integer>> A)
-    {
-        List<Integer> ans = new ArrayList<>();
-        int sum;
-        for (int i = 0; i < A.get(0).size(); i++)
-        {
-            sum = 0;
-            for (int j = 0; j < A.size(); j++)
-            {
-                sum += A.get(j).get(i);
-            }
-            ans.add(sum);
-        }
-        return ans;
     }
 }
